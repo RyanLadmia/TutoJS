e8d0c4e007845ca477260a3f6124f0d8ef861537
RyanLadmia <ryan.ladmia@laplateforme.io>
2024-12-20 14:28:49 +0100

    Installation de Node et Hono

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-const a =  1
+const { Hono } = require('hono');
+const app = new Hono();
+
+// Route de test
+app.get('/', (ctx) => {
+  return ctx.text('Bienvenue sur le serveur Hono!');
+});
+
+// Démarrer le serveur sur le port 3000
+app.listen(3000, () => {
+  console.log('Serveur Hono démarré sur http://localhost:3000');
+});
